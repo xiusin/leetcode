b3e6a7db2a9d54a69b08138221ce5e30d06777d2
Your Name <826466266@qq.com>
2019-12-11 22:19:42 +0800

    commit

--- editor/cn/189_RotateArray_test.go
+++ editor/cn/189_RotateArray_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 )
 
-//给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。 
+//给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
 //
-// 示例 1: 
+// 示例 1:
 //
 // 输入: [1,2,3,4,5,6,7] 和 k = 3
 //输出: [5,6,7,1,2,3,4]
@@ -15,22 +15,22 @@ import (
 //向右旋转 1 步: [7,1,2,3,4,5,6]
 //向右旋转 2 步: [6,7,1,2,3,4,5]
 //向右旋转 3 步: [5,6,7,1,2,3,4]
-// 
 //
-// 示例 2: 
+//
+// 示例 2:
 //
 // 输入: [-1,-100,3,99] 和 k = 2
 //输出: [3,99,-1,-100]
-//解释: 
+//解释:
 //向右旋转 1 步: [99,-1,-100,3]
-//向右旋转 2 步: [3,99,-1,-100] 
+//向右旋转 2 步: [3,99,-1,-100]
 //
-// 说明: 
+// 说明:
 //
-// 
-// 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。 
-// 要求使用空间复杂度为 O(1) 的 原地 算法。 
-// 
+//
+// 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+// 要求使用空间复杂度为 O(1) 的 原地 算法。
+//
 // Related Topics 数组
 
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -44,25 +44,16 @@ func rotate(nums []int, k int) {
 	//向右旋转 2 步: [6,7,1,2,3,4,5]
 	//向右旋转 3 步: [5,6,7,1,2,3,4]
 	var prevVal int
-	var started bool
 	for i, l := 0, len(nums); i < k; i++ {
 		for j := 1; j <= l; j++ {
 			// 记录要被覆盖的值
-			if !started {
-				prevVal = nums[j % l]
+			if j == 1 {
+				prevVal = nums[j%l]
 				nums[j%l] = nums[(j-1)%l]
-				started = true
 			} else {
 				nums[j%l], prevVal = prevVal, nums[j%l]
 			}
-			fmt.Println(nums)
 		}
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
-		fmt.Println()
-
-
 	}
 }
 
